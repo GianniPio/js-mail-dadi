6acfc5cad6dc795f92ceb02ba73c16b2c193d557
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-26 16:26:00 +0200

    correction and finished

--- mail/js/script.js
+++ mail/js/script.js
@@ -11,22 +11,27 @@ console.log(listParty);
 
 const userMail = prompt("Inserire la tua email...");
 
-// inserire un if che controlla che la mail dell'utente è dentro la lista
+// variabile per capire se la mail c'è
+let trovato = false;
+
+// inserire un for e un if che controlla che la mail dell'utente è dentro la lista
 
 for (let i = 0; i < listParty.length; i++) {
 
     // creo variabile che prende il valore i-esimo della lista
     var element = listParty[i];
     
-    // se la mail dell'utente è uguale ad una della lista, può entrare
+    // se la mail dell'utente è uguale ad una della lista la variabile "trovato" diventa true
     if (userMail == element) {
-        
-        console.log("Perfetto, puoi entrare al party");
-        break;
-
-        // altrimenti non può entrare
-    } else {
-
-        console.log("Non sei sulla lista, non puoi entrare");
+        trovato = true;
     }
-};
+}
+
+    
+    if (trovato == true) {
+        // se la variaibile è true stampa la mail è nella lista
+        console.log (userMail + ' Trovato --- Puoi entrare');
+    } else {
+        // altrimenti non è nella lista
+        console.log('Non trovato');
+    }
