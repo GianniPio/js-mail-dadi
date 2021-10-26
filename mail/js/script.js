17b87d89d9b64436418efac5ea4469d7a578ac2a
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-26 15:39:20 +0200

    control

--- mail/js/script.js
+++ mail/js/script.js
@@ -9,4 +9,24 @@ console.log(listParty);
 
 // l'utente scrive la propria mail
 
+const userMail = prompt("Inserire la tua email...");
+
 // inserire un if che controlla che la mail dell'utente è dentro la lista
+
+for (let i = 0; i < listParty.length; i++) {
+
+    // creo variabile che prende il valore i-esimo della lista
+    var element = listParty[i];
+    
+    // se la mail dell'utente è uguale ad una della lista, può entrare
+    if (userMail == element) {
+        
+        console.log("Perfetto, puoi entrare al party");
+        break;
+
+        // altrimenti non può entrare
+    } else {
+
+        console.log("Non sei sulla lista, non puoi entrare");
+    }
+};
