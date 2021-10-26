0ce0bfa81f58f9570bb7b3552e2981620320ab63
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-26 15:02:21 +0200

    list email

---
+++ mail/js/script.js
@@ -0,0 +1,12 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
+
+// generare un array contenente una lista di mail
+
+const listParty = ["leodavinci@gmail.com", "marcopolo@libero.it", "khaldrogo@westeros.dt", "walterwhiteww@gmail.com", "monkeydluffy@onepiece.com"];
+
+console.log(listParty);
+
+
+// l'utente scrive la propria mail
+
+// inserire un if che controlla che la mail dell'utente è dentro la lista
